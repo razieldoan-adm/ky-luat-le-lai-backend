0cd0e419522cf77b9d2f28350551dd1ac58f9573
Thai Huy <raziel.doan@gmail.com>
2025-07-22 13:59:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://ky-luat-le-lai-frontend.vercel.app'], // domain frontend vercel của bạn
+  credentials: true,
+}));
 app.use(express.json());
 
 
