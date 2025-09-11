db345a0635a25906d444564742287d927eedf75a
Thai Huy <raziel.doan@gmail.com>
2025-09-11 20:11:45 +0700

    Update classWeeklyScoreRoutes.js

--- routes/classWeeklyScoreRoutes.js
+++ routes/classWeeklyScoreRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const weeklyScoresController = require('../controllers/classWeeklyScoreController');
+const controller = require('../controllers/classWeeklyScoreController');
 
-router.get('/', weeklyScoresController.getWeeklyScores);
-router.post('/', weeklyScoresController.saveWeeklyScores);
-router.post('/calculate', weeklyScoresController.calculateWeeklyScores);
-router.post('/calculate-total-rank', weeklyScoresController.calculateTotalRank);
+// Save mới
+router.post('/', controller.saveScore);
+
+// Update lại
+router.put('/:id', controller.updateScore);
 
 module.exports = router;
