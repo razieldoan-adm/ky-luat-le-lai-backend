e7d3e41e5c19ec3b7aeaf937ec910d2c5a62ebe8
Thai Huy <raziel.doan@gmail.com>
2025-09-22 21:26:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //API cần sử dụng
 app.use('/api/violations', violationRoutes);
