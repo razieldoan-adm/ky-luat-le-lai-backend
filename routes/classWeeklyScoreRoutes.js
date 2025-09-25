c3d8a20bed6ec4c1e028c5ed3bbb73fcee30f4c3
Thai Huy <raziel.doan@gmail.com>
2025-09-25 23:20:56 +0700

    Update classWeeklyScoreRoutes.js

--- routes/classWeeklyScoreRoutes.js
+++ routes/classWeeklyScoreRoutes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 const classWeeklyScoreController = require("../controllers/classWeeklyScoreController");
 
+// lấy dữ liệu đã lưu
 router.get("/", classWeeklyScoreController.getWeeklyScores);
+
+// lấy dữ liệu thô theo tuần
 router.get("/temp/:weekNumber", classWeeklyScoreController.getTempWeeklyScores);
+
+// lưu dữ liệu lần đầu
 router.post("/save", classWeeklyScoreController.saveWeeklyScores);
+
+// cập nhật lại toàn bộ điểm + xếp hạng
 router.put("/update/:weekNumber", classWeeklyScoreController.updateWeeklyScores);
 
 module.exports = router;
