3ee29a4c31f96455d32b8f2328f3598d27c35972
Thai Huy <raziel.doan@gmail.com>
2025-09-26 18:54:46 +0700

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  normalizedName: String,
   className: { type: String, required: true },
   fatherPhone: { type: String, default: '' },
   motherPhone: { type: String, default: '' }
 });
+// middleware để luôn cập nhật normalizedName
+studentSchema.pre("save", function (next) {
+  if (this.name) {
+    this.normalizedName = removeVietnameseTones(this.name).toLowerCase();
+  }
+  next();
+})
 
 module.exports = mongoose.model('Student', studentSchema);
