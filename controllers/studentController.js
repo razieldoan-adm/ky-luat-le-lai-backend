3d400b48e2250cf6dfe9d0c938aaab93ee9ca9a4
Thai Huy <raziel.doan@gmail.com>
2025-09-29 21:53:05 +0700

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -101,13 +101,6 @@ exports.updatePhones = async (req, res) => {
 };
 // GET /api/students/search?name=abc
 // controllers/studentController.js
-const removeVietnameseTones = (str) => {
-  return str
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/đ/g, "d").replace(/Đ/g, "D")
-    .replace(/[^a-zA-Z0-9\s]/g, "");
-};
 
 exports.searchStudents = async (req, res) => {
   try {
