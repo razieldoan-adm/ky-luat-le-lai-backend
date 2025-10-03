f64d2275ebf28e0c5be8dbe9280513e33ee237e4
Thai Huy <raziel.doan@gmail.com>
2025-10-03 23:50:14 +0700

    Update ClassLineUpSummary.js

--- models/ClassLineUpSummary.js
+++ models/ClassLineUpSummary.js
@@ -1,14 +1,11 @@
-// models/ClassLineUpSummary.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const classLineUpSummarySchema = new mongoose.Schema({
+const ClassLineUpSummarySchema = new mongoose.Schema({
   className: { type: String, required: true },
   grade: { type: String, required: true },
   weekNumber: { type: Number, required: true },
-  data: { type: [Number], required: true }, // Mảng 10 buổi
-  total: { type: Number, required: true },
-}, { timestamps: true });
+  scores: { type: [Number], default: Array(10).fill(0) }, // 10 cột điểm
+  total: { type: Number, default: 0 }
+});
 
-classLineUpSummarySchema.index({ className: 1, weekNumber: 1 }, { unique: true });
-
-module.exports = mongoose.model('ClassLineUpSummary', classLineUpSummarySchema);
+module.exports = mongoose.model("ClassLineUpSummary", ClassLineUpSummarySchema);
