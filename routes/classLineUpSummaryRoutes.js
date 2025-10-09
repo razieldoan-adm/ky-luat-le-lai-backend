b510c5070adf45858b06c032c4720b09ae659cb0
Thai Huy <raziel.doan@gmail.com>
2025-10-09 21:23:48 +0700

    Update classLineUpSummaryRoutes.js

--- routes/classLineUpSummaryRoutes.js
+++ routes/classLineUpSummaryRoutes.js
@@ -6,4 +6,10 @@ router.post('/', controller.createRecord);
 router.get('/weekly-summary', controller.getWeeklySummary);
 router.delete('/:id', controller.deleteRecord);
 router.get('/', controller.getAllRecords);
+
+// ✅ Tổng hợp điểm theo lớp
+router.get("/summary-by-class", controller.getClassLineUpTotal);
+
+// ✅ Ghi điểm vào ClassWeeklyScore
+router.post("/update-weekly-lineup", controller.updateWeeklyLineUpScore);
 module.exports = router;
