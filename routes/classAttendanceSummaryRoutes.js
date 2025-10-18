b14a31a9fc7104e87d4ed1f44c3218dca8270d01
Thai Huy <raziel.doan@gmail.com>
2025-10-19 01:09:13 +0700

    Update classAttendanceSummaryRoutes.js

--- routes/classAttendanceSummaryRoutes.js
+++ routes/classAttendanceSummaryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const attendanceController = require("../controllers/attendanceController");
+const attendanceController = require("../controllers/classAttendanceSummaryController");
 
 // Ghi nhận học sinh nghỉ học
 router.post("/record", attendanceController.recordAbsence);
