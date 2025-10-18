3ec8e132b3b51f796185795261ee4cce61ed7d0a
Thai Huy <raziel.doan@gmail.com>
2025-10-19 00:56:11 +0700

    Update ClassAttendanceSummary.js

--- models/ClassAttendanceSummary.js
+++ models/ClassAttendanceSummary.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const ClassAttendanceSummarySchema = new mongoose.Schema({
-  studentName: { type: String, required: true },
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
   className: { type: String, required: true },
   grade: { type: String, required: true },
+  studentName: { type: String, required: true },
+  date: { type: String, required: true }, // yyyy-mm-dd
+  session: { type: String, enum: ["Sáng", "Chiều"], required: true },
+  permission: { type: Boolean, default: false }, // false = không phép
   weekNumber: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
-  permission: { type: Boolean, default: false }, // false = nghỉ không phép, true = có phép
 });
 
 module.exports = mongoose.model("ClassAttendanceSummary", ClassAttendanceSummarySchema);
