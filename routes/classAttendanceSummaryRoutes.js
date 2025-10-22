5a265a5587b0c92a428535c1bc86a304f4b36e81
Thai Huy <raziel.doan@gmail.com>
2025-10-22 21:24:00 +0700

    Update classAttendanceSummaryRoutes.js

--- routes/classAttendanceSummaryRoutes.js
+++ routes/classAttendanceSummaryRoutes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const attendanceController = require("../controllers/classAttendanceSummaryController");
+const attendanceController = require("../controllers/attendanceController");
 
 // Ghi nhận nghỉ học
 router.post("/", attendanceController.recordAbsence);
 
-// Lấy danh sách
+// Lấy theo ngày / tuần
 router.get("/by-date", attendanceController.getByDate);
 router.get("/by-week", attendanceController.getByWeek);
 
-// Duyệt phép & xác nhận
-router.put("/approve/:id", attendanceController.approvePermission);
-router.put("/confirm/:id", attendanceController.confirmAbsence);
-
-// Lấy danh sách nghỉ không phép
+// Lấy danh sách nghỉ học không phép
 router.get("/unexcused", attendanceController.getUnexcusedAbsences);
+
+// Duyệt phép / xác nhận phép
+router.put("/approve/:id", attendanceController.approvePermission);
+router.put("/confirm/:id", attendanceController.confirmPermission);
 
 module.exports = router;
