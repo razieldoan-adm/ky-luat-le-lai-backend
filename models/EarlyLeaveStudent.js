dddc67f93a68a22aa910af1dadb32b0444cfdc03
Thai Huy <raziel.doan@gmail.com>
2026-01-18 23:38:14 +0700

    Update EarlyLeaveStudent.js

--- models/EarlyLeaveStudent.js
+++ models/EarlyLeaveStudent.js
@@ -5,17 +5,16 @@ const earlyLeaveStudentSchema = new mongoose.Schema(
     name: { type: String, required: true },
     normalizedName: { type: String, required: true },
     className: { type: String, required: true },
-    schoolYear: { type: String, required: true }, // ví dụ: 2025-2026
   },
   { timestamps: true }
 );
 
-// ⚡ Load nhanh theo lớp + năm
-earlyLeaveStudentSchema.index({ className: 1, schoolYear: 1 });
+// ⚡ Load nhanh theo lớp
+earlyLeaveStudentSchema.index({ className: 1 });
 
-// ❌ Không cho trùng HS trong cùng lớp + năm
+// ❌ Không cho trùng HS trong cùng lớp
 earlyLeaveStudentSchema.index(
-  { normalizedName: 1, className: 1, schoolYear: 1 },
+  { normalizedName: 1, className: 1 },
   { unique: true }
 );
 
