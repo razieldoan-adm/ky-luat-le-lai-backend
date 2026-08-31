2d6c7ed3e1aa7edf15fe40a49c1f06713a2b380a
Thai Huy <raziel.doan@gmail.com>
2026-08-31 22:46:21 +0700

    Update ClassViolationScore.js

--- models/ClassViolationScore.js
+++ models/ClassViolationScore.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const classViolationScoreSchema = new mongoose.Schema({
   className: { type: String, required: true }, // ✅ chỉ cần className
+  academicYear: {type: String, required: true,},
   weekNumber: { type: Number, required: true },
   totalScore: { type: Number, default: 0 },
 });
