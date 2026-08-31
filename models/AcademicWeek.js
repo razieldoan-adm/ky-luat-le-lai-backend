eea8b7268df29bbd39108103b1aa02b93488421d
Thai Huy <raziel.doan@gmail.com>
2026-08-31 14:24:33 +0700

    Update AcademicWeek.js

--- models/AcademicWeek.js
+++ models/AcademicWeek.js
@@ -4,6 +4,7 @@ const academicWeekSchema = new mongoose.Schema({
 
   startDate: Date,
   endDate: Date,
+  academicYear: { type: String, required: true },
   weekNumber: {type: Number, default: null},
   isStudyWeek: { type: Boolean, default: false },
 });
