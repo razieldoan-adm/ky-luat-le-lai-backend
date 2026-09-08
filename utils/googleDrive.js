08bbd5d69bbe997f13ec7459d0706dce65326540
Thai Huy <raziel.doan@gmail.com>
2026-09-08 19:01:40 +0700

    Update googleDrive.js

--- utils/googleDrive.js
+++ utils/googleDrive.js
@@ -10,7 +10,11 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 );
-
+if (process.env.GOOGLE_REFRESH_TOKEN) {
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  });
+}
 // ============================================================
 // GOOGLE DRIVE CLIENT
 // ============================================================
