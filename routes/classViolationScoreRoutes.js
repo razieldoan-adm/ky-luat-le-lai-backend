dc160c3ccba1753423e59591cbf9ee3872ce7562
Thai Huy <raziel.doan@gmail.com>
2026-09-23 15:41:19 +0700

    Update classViolationScoreRoutes.js

--- routes/classViolationScoreRoutes.js
+++ routes/classViolationScoreRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const classViolationScoreController = require('../controllers/classViolationScoreController');
 
 router.post('/', classViolationScoreController.createOrUpdateClassViolationScore);
+
+// API tính điểm vi phạm theo tuần
+router.get('/calculate', classViolationScoreController.calculateClassViolationScores);
+
 router.get('/:classId/:weekId', classViolationScoreController.getClassViolationScore);
 router.get('/', classViolationScoreController.getByWeek);
 
